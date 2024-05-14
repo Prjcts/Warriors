d3aede19cbc66d5f0e8afb35bcf5e8c84ad451ec
LukaMazione <luka.mazione@gmail.com>
2024-05-14 21:33:33 +0100

    fameofhall

--- routes/hallOfFame-route.ts
+++ routes/hallOfFame-route.ts
@@ -1,10 +1,20 @@
-import { Router } from 'express';
+import {Request, Response, Router} from 'express';
+import {Warrior} from '../models/warrior.record';
 
 const hallOfFameRouter = Router();
 
 hallOfFameRouter
-.get('/fame', (req, res): void => {
-  res.render('hall-of-fame')
-});
+	.get('/fame', async (req: Request, res: Response) => {
+		const topTen = (await Warrior.getTop(5)).map((warrior, index) => (
+			{
+				place: index + 1,
+				warrior,
+			}
+		));
+		console.log(topTen);
+		res.render('hall-of-fame/list', {
+			topTen,
+		});
+	});
 
-export { hallOfFameRouter };
+export {hallOfFameRouter};
