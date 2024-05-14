b82adc0ede23b9afed3e68933458f7c0dd0943c1
LukaMazione <luka.mazione@gmail.com>
2024-05-14 22:30:25 +0100

    somechanges

--- routes/hallOfFame-route.ts
+++ routes/hallOfFame-route.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
+import {Router} from 'express';
 
 const hallOfFameRouter = Router();
 
 hallOfFameRouter
-.get('/fame', (req, res): void => {
-  res.render('hall-of-fame')
-});
+	.get('/fame', (req, res): void => {
+		res.render('hall-of-fame');
+	});
 
-export { hallOfFameRouter };
+export {hallOfFameRouter};
--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,11 @@
-import { createPool } from 'mysql2/promise';
+import {createPool} from 'mysql2/promise';
 
 const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'warriors',
-  namedPlaceholders: true,
-  decimalNumbers: true,
+	host: 'localhost',
+	user: 'root',
+	database: 'warriors',
+	namedPlaceholders: true,
+	decimalNumbers: true,
 });
 
-export {pool}
+export {pool};
